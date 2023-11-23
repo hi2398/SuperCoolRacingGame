bb3e6190aa92d0f529bb5f771614b04bcb68052a
Pascal Hirt <79571006+hi2398@users.noreply.github.com>
2023-11-23 19:34:01 +0100

    add Unreal Project

---
+++ SuperCoolRacingGame/Source/SuperCoolRacingGame/Public/RacingKartPawn.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/DefaultPawn.h"
+#include "RacingKartPawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUPERCOOLRACINGGAME_API ARacingKartPawn : public ADefaultPawn
+{
+	GENERATED_BODY()
+	
+};
